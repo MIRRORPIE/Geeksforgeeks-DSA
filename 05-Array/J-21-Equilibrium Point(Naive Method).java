54795da7fdfbb590a0ed14f254b0fbb8bd425c33
unknown <raushankumar1047@pec.edu>
2020-10-22 19:02:45 +0530

    array tutorials

---
+++ 05-Array/J-21-Equilibrium Point(Naive Method).java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class GFG 
+{ 
+
+  
+    static boolean checkEquilibrium(int arr[], int n)
+    {
+    	for(int i  = 0; i < n; i++)
+    	{
+    		int l_sum = 0, r_sum = 0;
+
+    		for(int j = 0; j < i; j++)
+    			l_sum += arr[j];
+
+    		for(int j = i + 1; j < n; j++)
+    			r_sum += arr[j];
+
+    		if(l_sum == r_sum)
+    			return true;
+    	}
+
+    	return false;
+    }
+    public static void main(String args[]) 
+    { 
+       int arr[] = {3, 4, 8, -9, 20, 6}, n = 6;
+
+       System.out.println(checkEquilibrium(arr, n));
+
+    } 
+
+}

bca2efc72091cd862331981d6b825d59806cab08
unknown <raushankumar1047@pec.edu>
2020-10-27 00:03:58 +0530

    Searching tutorials

--- 06-Searching/C-11-pair in unsorted array which gives sum X(Naive).cpp
+++ 06-Searching/C-11-pair in unsorted array which gives sum X(Naive).cpp
@@ -1,32 +1,32 @@
-// C++ program to check if given array 
-// has 2 elements whose sum is equal 
-// to the given value 
-#include <bits/stdc++.h> 
+// C++ program to check if given array
+// has 2 elements whose sum is equal
+// to the given value
+#include <bits/stdc++.h>
 
-using namespace std; 
+using namespace std;
 
-void printPairs(int arr[], int arr_size, int sum) 
-{ 
-	unordered_set<int> s; 
-	for (int i = 0; i < arr_size; i++) { 
-		int temp = sum - arr[i]; 
+void printPairs(int arr[], int arr_size, int sum)
+{
+	unordered_set<int> s;
+	for (int i = 0; i < arr_size; i++) {
+		int temp = sum - arr[i];
 
-		if (s.find(temp) != s.end()) 
-			cout << "Pair with given sum " << sum << " is (" << arr[i] << ", " << temp << ")" << endl; 
+		if (s.find(temp) != s.end())
+			cout << "Pair with given sum " << sum << " is (" << arr[i] << ", " << temp << ")" << endl;
 
-		s.insert(arr[i]); 
-	} 
-} 
+		s.insert(arr[i]);
+	}
+}
 
 /* Driver program to test above function */
-int main() 
-{ 
-	int A[] = { 1, 4, 45, 6, 10, 8 }; 
-	int n = 16; 
-	int arr_size = sizeof(A) / sizeof(A[0]); 
+int main()
+{
+	int A[] = { 12, 4, 45, 6, 12, 4, 8, 8, 10, 8 };
+	int n = 16;
+	int arr_size = sizeof(A) / sizeof(A[0]);
 
-	// Function calling 
-	printPairs(A, arr_size, n); 
+	// Function calling
+	printPairs(A, arr_size, n);
 
-	return 0; 
-} 
+	return 0;
+}
